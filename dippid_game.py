89686c8a312e826eb8ea3ef4c4a75ea2c686e121
michaelmeckl <Michael.Meckl@stud.uni-regensburg.de>
2021-06-13 18:54:14 +0200

    Add levels to the game and improve controls

--- dippid_game.py
+++ dippid_game.py
@@ -8,11 +8,9 @@ Implemented by Michael Meckl.
 import sys
 import os
 from argparse import ArgumentParser
-from DIPPID import SensorUDP
-from PyQt5 import QtWidgets, QtCore, uic, QtGui
-from PyQt5.QtGui import QPainter
-from game_widget import GameWindow, Direction
-
+import DIPPID
+from PyQt5 import QtWidgets, QtCore, uic
+from game_widget import Direction
 
 # CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 # ui_file = os.path.join(CURRENT_DIR, "dippid_game.ui")
@@ -25,38 +23,81 @@ class DippidGame(QtWidgets.QWidget):
 
     def __init__(self, port=5700):
         super(DippidGame, self).__init__()
-        self.game_started = False
+        self.sensor = DIPPID.SensorUDP(port)
+
         # self.setupUi(self)
-        self.sensor = SensorUDP(port)
-
-        # self.game = GameWindow()
-
         self.ui = uic.loadUi("dippid_game.ui", self)
         self._show_introduction()
 
     def _show_introduction(self):
         self.ui.stackedWidget.setCurrentIndex(0)
-
-        # TODO would be better to check this in an infinite loop in the background? or at least until connected! maybe don't even allow start game?
-        self._check_connected_status()
-
         self.ui.btn_start_game.setFocusPolicy(QtCore.Qt.NoFocus)  # prevent auto-focus of the start button
+        self.ui.btn_start_game.setEnabled(False)  # disable the start game button until the sensor device connected!
+
+        # check the connection to the device repeatedly until successful connection; without the connection
+        # the game can't be played so it would be useless to allow a user to start it
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self._check_connected_status)
+        self.timer.start(10)  # wait for 10 ms until next try
+
         self.ui.btn_start_game.clicked.connect(self._show_game)
 
+    def _check_connected_status(self):
+        if self._is_connected():
+            self.ui.connected_status.setStyleSheet("QLabel { font-weight: bold; color : green;}")
+            self.ui.connected_status.setText("Connected")
+            self.ui.btn_start_game.setEnabled(True)
+        else:
+            self.ui.connected_status.setStyleSheet("QLabel { font-weight: bold; color : red;}")
+            self.ui.connected_status.setText("Not connected")
+            self.ui.btn_start_game.setEnabled(False)
+
+    def _is_connected(self) -> bool:
+        # TODO find a better method to test connection!
+        #  (if we have been connected once it won't recognize a connection loss as the capabilities are already there!)
+        # check if all capabilities have been registered (if all work the sensor is obviously sending data)
+        capabilities_ready = all(self.sensor.has_capability(capability) for capability in DippidGame.ALL_CAPABILITIES)
+        return True if capabilities_ready else False
+
+    def _show_game(self):
+        self.timer.stop()  # stop the qtimer that checks whether we are connected or not (we have to be if we are here)
+
+        index = self.ui.stackedWidget.currentIndex() + 1
+        # switch widget index to the element in the stack at the given index (i.e. move to this page)
+        self.ui.stackedWidget.setCurrentIndex(index)
+        self._start_game()
+
+    def _start_game(self):
+        self.ui.game_widget.start(level_finished_callback=self._update_level,
+                                  points_changed_callback=self._update_points)
+
+        # the callbacks need to be registered AFTER checking connected status and starting the game, otherwise we can't
+        # be sure about the connected status as they register themselves as capabilities as well (and would fire before
+        # the game even started)
+        self._register_sensor_callbacks()
+
+        # print('capabilities: ', self.sensor.get_capabilities())
+        # if self.sensor.has_capability("accelerometer"):
+        #    print("accelerometer x-val:", self.sensor.get_value("accelerometer")['x'])
+
+    def _update_level(self, level: int):
+        self.ui.level.setText(str(level))
+
+    def _update_points(self, new_points: int):
+        self.ui.points.setText(str(new_points))
+
     def _register_sensor_callbacks(self):
-        self.sensor.register_callback('button_1', self._handle_button_press)  # TODO can button be useful as well?
+        # self.sensor.register_callback('button_1', self._handle_button_press)
         # self.sensor.register_callback('accelerometer', self._handle_movement)
         self.sensor.register_callback('gravity', self._handle_movement)
         self.sensor.register_callback('gyroscope', self._handle_position_change)
 
-    # TODO
     def _handle_acceleration(self, data):
         # the mobile device was accelerated in some direction!
         if data["x"] < -0.60:
             # self.last_accel = data["x"]
             self.ui.game_widget.move_character_forward()
 
-    # TODO doch while schleife probieren?
     def _handle_movement(self, data):
         # the mobile device is tilted in a specific direction!
         if data["x"] < -6.0:
@@ -77,42 +118,6 @@ class DippidGame(QtWidgets.QWidget):
                 print('button pressed')
         except Exception as e:
             sys.stderr.write(f"Something went wrong when trying to cast button data: {e}")
-
-    def _check_connected_status(self):
-        if self._is_connected():
-            self.ui.connected_status.setStyleSheet("QLabel { font-weight: bold; color : green;}")
-            self.ui.connected_status.setText("Connected")
-        else:
-            self.ui.connected_status.setStyleSheet("QLabel { font-weight: bold; color : red;}")
-            self.ui.connected_status.setText("Not connected")
-
-    def _is_connected(self):
-        # check if all capabilities have been registered (if all work the sensor is obviously sending data)
-        capabilities_ready = all(self.sensor.has_capability(capability) for capability in DippidGame.ALL_CAPABILITIES)
-        return True if capabilities_ready else False
-
-    def _show_game(self):
-        index = self.ui.stackedWidget.currentIndex() + 1
-        # switch widget index to the element in the stack at the given index (i.e. move to this page)
-        self.ui.stackedWidget.setCurrentIndex(index)
-        self._start_game()
-
-    def _start_game(self):
-        self.game_started = True
-        self.ui.game_widget.start()
-
-        # the callbacks need to be registered AFTER checking connected status, otherwise we can't be sure about the
-        # connected status as they register themselves as capabilities as well
-        self._register_sensor_callbacks()
-
-        print('capabilities: ', self.sensor.get_capabilities())
-        # if self.sensor.has_capability("accelerometer"):
-        #    print("accelerometer x-val:", self.sensor.get_value("accelerometer")['x'])
-
-    def keyPressEvent(self, key: QtGui.QKeyEvent):
-        pass
-        # if
-        #    self.game_started = False
 
     # def closeEvent(self, event: QtGui.QCloseEvent):
     #     self.sensor.disconnect()  # stop sensor before closing!
--- game_widget.py
+++ game_widget.py
@@ -5,21 +5,24 @@
 Implemented by Michael Meckl.
 """
 
+import sys
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPainter, QPen, QBrush, QPaintEvent
+from PyQt5.QtGui import QPainter, QPen, QBrush, QPaintEvent, QColor
 from enum import Enum
 
 
 Direction = Enum("Direction", "UP DOWN")
 
 
+# noinspection PyAttributeOutsideInit
 class GameWindow(QtWidgets.QFrame):
 
     def __init__(self, *args, **kwargs):
         super(GameWindow, self).__init__(*args, **kwargs)
-        self.setStyleSheet("border: 1px solid black; background-color: rgb(211, 227, 248);")
-        self.setFixedSize(700, 350)  # necessary to set fixed size as otherwise we can't know height and width before the first draw!
+        self.setStyleSheet("border: 1px solid black; background-color: rgb(227, 219, 155);")
+        # necessary to set fixed size as otherwise we can't know dimensions before the first draw!
+        self.setFixedSize(700, 350)
 
         # print("At start:", self.width(), self.height())
         self.__width, self.__height = self.width(), self.height()
@@ -31,111 +34,193 @@ class GameWindow(QtWidgets.QFrame):
         self.player_start_xPos = 5
         self.player_yPos_top_lane = self.inset + self.road_height / 4
         self.player_yPos_bottom_lane = self.inset + self.road_height + self.road_height / 4
-        self.player_velocity = 0.0
 
-        self.set_initial_values()
+        self._setup_roads()
+        self._setup_levels()
 
-    def set_initial_values(self):
-        # TODO draw them instead of giving to main class and setting in ui?
+    def _setup_roads(self):
+        y_middle, y_bottom = self.inset + self.road_height, self.inset + self.road_height * 2
+        self.sideline_top = (0, self.inset, self.__width, self.inset)
+        self.middle_line = (0, y_middle, self.__width, y_middle)
+        self.sideline_bottom = (0, y_bottom, self.__width, y_bottom)
+
+    def _setup_levels(self):
+        self.obstacle_radius = self.road_height / 2 - 5
+        self.obstacle_top_row_y = int(self.inset + self.obstacle_radius + 5)
+        self.obstacle_bottom_row_y = int(self.obstacle_top_row_y + self.road_height)
+
+        self.collectible_radius = 10
+        self.collectible_top_row_y = int(self.inset + self.road_height / 2)
+        self.collectible_bottom_row_y = int(self.collectible_top_row_y + self.road_height)
+
+        self.levels = {
+            1: {
+                "obstacles": [(self.__width - 300, self.obstacle_top_row_y)],
+                "collectibles": [(self.__width - 300, self.collectible_bottom_row_y),
+                                 (self.__width - 130, self.collectible_top_row_y),
+                                 (self.__width - 500, self.collectible_top_row_y),
+                                 (self.__width - 450, self.collectible_bottom_row_y)]
+            },
+            2: {
+                "obstacles": [(self.__width - 480, self.obstacle_top_row_y),
+                              (self.__width - 270, self.obstacle_bottom_row_y)],
+                "collectibles": [(self.__width - 560, self.collectible_top_row_y),
+                                 (self.__width - 430, self.collectible_top_row_y),
+                                 (self.__width - 180, self.collectible_bottom_row_y)]
+            },
+            3: {
+                "obstacles": [(self.__width - 520, self.obstacle_top_row_y),
+                              (self.__width - 385, self.obstacle_bottom_row_y),
+                              (self.__width - 250, self.obstacle_top_row_y),
+                              (self.__width - 145, self.obstacle_bottom_row_y)],
+                "collectibles": [(self.__width - 390, self.collectible_top_row_y),
+                                 (self.__width - 205, self.collectible_top_row_y),
+                                 (self.__width - 422, self.collectible_bottom_row_y),
+                                 (self.__width - 75, self.collectible_bottom_row_y)]
+            }
+        }
+
+    def start(self, level_finished_callback, points_changed_callback):
+        # set callbacks to notify the ui outside the game window
+        self.__level_callback = level_finished_callback
+        self.__points_callback = points_changed_callback
+
+        # init the first level
+        self._set_initial_values()
+        self._init_first_level()
+
+    def _set_initial_values(self):
         self.current_level = 1
         self.current_points = 0
-
-        self.current_obstacles = []
 
         self.at_top_lane = True
         self.player_xPos, self.player_yPos = self.player_start_xPos, self.player_yPos_top_lane
 
-    def start(self):
-        print("Game Window started")
+    def _init_first_level(self):
+        self.current_obstacles = []
+        self.current_collectibles = []
+        self._set_values_for_level(level_index=1)
 
-    def update_player_pos(self, new_x, new_y):
-        self.player_xPos, self.player_yPos = new_x, new_y
-
-    def update_player_velocity(self, new_vel):
-        self.player_velocity = new_vel
+    def _set_values_for_level(self, level_index: int):
+        try:
+            level = self.levels[level_index]
+        except IndexError:
+            sys.stderr.write(f"Tried to access level that doesn't exist (index={level_index}!")
+            return
+        self.current_obstacles = level.get("obstacles")
+        self.current_collectibles = level.get("collectibles")
 
     def move_character_forward(self):
         self.player_xPos += self.player_width / 2
+        self.repaint()
+
         if self.player_xPos > self.__width:
             print("Level finished!")
-            self.current_level += 1
-            self.current_points += 100
-            self.load_next_level()
+            self._level_up()
+        else:
+            # check if player collided with an obstacle or a collectible
+            self._check_player_collision()
 
-    def load_next_level(self):
-        # TODO get obstacle positions for this level! define all at the start in a dict per level!
+    def _level_up(self):
+        self.current_level += 1
+        self.current_points += 100
+
+        # load next level if there is one
+        self._load_next_level()
+        # and notify callbacks
+        self.__level_callback(self.current_level)
+        self.__points_callback(self.current_points)
+
+    def _load_next_level(self):
+        if self.current_level > len(self.levels):
+            # start at the first level again when no others left
+            self.current_level = 1
+
+        self._set_values_for_level(level_index=self.current_level)
         self.player_xPos = self.player_start_xPos
-        # self.repaint()
-        self.update()
 
-    def get_player_bounds(self):
+    def __get_player_bounds(self):
         return self.player_xPos, self.player_yPos, self.player_xPos + self.player_width, self.player_yPos + self.player_height
 
-    def check_obstacle_hit(self):
-        pass  # TODO
+    def _check_player_collision(self):
+        self.__check_collectible_hit()
+        self.__check_obstacle_hit()
 
-    def check_collectible_hit(self):
-        pass  # TODO
+    def __check_obstacle_hit(self):
+        # TODO check bounding boxes for player and obstable, if hit remove points and notify callback
+        pass
+
+    def __check_collectible_hit(self):
+        # TODO check bounding boxes for player and collectible, if hit add points and notify callback;
+        #  also remove this collectible from the current_collectibles list
+        pass
 
     def switch_lane(self, direction: Direction):
         if direction == Direction.UP and not self.at_top_lane:
+            # move to the top lane
             self.at_top_lane = True
             self.player_yPos = self.player_yPos_top_lane
+
+            self._check_player_collision()
         elif direction == Direction.DOWN and self.at_top_lane:
+            # move to the bottom lane
             self.at_top_lane = False
             self.player_yPos = self.player_yPos_bottom_lane
-        else:
-            print("Switching lane not working! Already at this lane!")
 
-        # self.repaint()
-        self.update()
+            self._check_player_collision()
+        else:
+            print("Switching lane did not work! Player is already at this lane!")
+
+        self.repaint()
+        # self.update()
 
     def paintEvent(self, event: QPaintEvent):
-        # super().paintEvent(event)
         painter = QPainter()
         painter.begin(self)
-        painter.setRenderHints(painter.Antialiasing)
+        # painter.setRenderHints(painter.Antialiasing)
 
-        self.draw_roads(painter)
-        self.draw_obstacles(painter)
-        self.draw_collectibles(painter)
-        self.draw_player(painter)
+        # draw all parts of the game; order does matter!
+        self._draw_roads(painter)
+        self._draw_obstacles(painter)
+        self._draw_collectibles(painter)
+        self._draw_player(painter)
 
         painter.end()
 
-    def draw_roads(self, painter: QPainter):
-        y1, y2, y3 = self.inset, self.inset + self.road_height, self.inset + self.road_height * 2
-        sideline_top = (0, y1, self.width(), y1)
-        middle_line = (0, y2, self.width(), y2)
-        sideline_bottom = (0, y3, self.width(), y3)
+    def _draw_roads(self, painter: QPainter):
+        # fill background of road first
+        painter.setBrush(QBrush(QColor(186, 186, 186), Qt.SolidPattern))
+        painter.drawRect(0, self.inset, self.__width, self.road_height * 2)
 
+        # draw road lines
         pen = QPen(Qt.black, 3, Qt.SolidLine)
         painter.setPen(pen)
-        painter.drawLine(*sideline_top)
-        painter.drawLine(*sideline_bottom)
+        painter.drawLine(*self.sideline_top)
+        painter.drawLine(*self.sideline_bottom)
         pen.setStyle(Qt.DashLine)
         painter.setPen(pen)
-        painter.drawLine(*middle_line)
+        painter.drawLine(*self.middle_line)
 
-    def draw_collectibles(self, painter: QPainter):
-        painter.setPen(QPen(Qt.NoPen))
+    def _draw_collectibles(self, painter: QPainter):
+        painter.setPen(QPen(Qt.NoPen))  # set to NoPen so no outline will be drawn
         painter.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
-        painter.drawEllipse(self.width() - 300, int(self.inset + self.road_height / 3), 20, 20)
+        for collectible_pos in self.current_collectibles:
+            painter.drawEllipse(QtCore.QPoint(*collectible_pos), self.collectible_radius, self.collectible_radius)
 
-    def draw_obstacles(self, painter: QPainter):
-        pen = QPen(Qt.NoPen)  # set to NoPen so no outline will be drawn
-        painter.setPen(pen)
+    def _draw_obstacles(self, painter: QPainter):
+        painter.setPen(QPen(Qt.NoPen))
         painter.setBrush(QBrush(Qt.black, Qt.SolidPattern))
+        for obstacle_pos in self.current_obstacles:
+            painter.drawEllipse(QtCore.QPoint(*obstacle_pos), self.obstacle_radius, self.obstacle_radius)
 
-        circle_radius = self.road_height / 2 - 5
-        painter.drawEllipse(QtCore.QPoint(self.width() - 300, int(self.inset + self.road_height + circle_radius + 5)), circle_radius, circle_radius)
-
-    def draw_player(self, painter: QPainter):
-        # body
+    def _draw_player(self, painter: QPainter):
+        # draw body
         painter.setPen(QPen(Qt.NoPen))
         painter.setBrush(QBrush(Qt.darkGreen, Qt.SolidPattern))
         painter.drawRect(self.player_xPos, self.player_yPos, self.player_width, self.player_height)
-        # eyes and mouth
+
+        # draw eyes and mouth afterwards
         painter.setPen(QPen(Qt.black, 4, Qt.SolidLine))
         painter.drawPoint(self.player_xPos + self.player_width - 5, self.player_yPos + 5)
-        painter.drawLine(self.player_xPos + self.player_width - 8, self.player_yPos + 10, self.player_xPos + self.player_width, self.player_yPos + 10)
+        painter.drawLine(self.player_xPos + self.player_width - 8, self.player_yPos + 10,
+                         self.player_xPos + self.player_width, self.player_yPos + 10)
